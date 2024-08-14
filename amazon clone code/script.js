7fe2affeb7f09c136e34d31d19c7c99bb26b25db
Shubhlak <shubhlakkanpate@gmail.com>
2024-08-14 22:33:46 +0530

    First - commit

---
+++ amazon clone code/script.js
@@ -0,0 +1,41 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const control_prev = document.querySelector('.control_prev');
+const control_next = document.querySelector('.control_next');
+
+
+let n = 0;
+
+function changeSlide(){
+    for(let i = 0; i < imgs.length;i++){
+        imgs[i].style.display='none';
+    }
+    imgs[n].style.display='block';
+}
+changeSlide();
+
+control_prev.addEventListener('click', (e)=>{
+    if(n>0){
+        n--;
+    }else{
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+control_next.addEventListener('click', (e)=>{
+    if(n<imgs.length - 1){
+        n++;
+    }else{
+        n =0;
+    }
+    changeSlide();
+})
+
+
+const scrollContaner = document.querySelectorAll('.products');
+for(const item of scrollContaner){
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    }); 
+
+}
